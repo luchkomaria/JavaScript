96ea8821040d366587759287edce7d44e94c414f
luchkomaria <luchkomaria@gmail.com>
2017-05-13 19:25:28 +0300

    sdf

---
+++ JS_9/index1.php
@@ -0,0 +1,28 @@
+﻿<?php
+$name = $_POST["name"];
+$password = $_POST["password"];
+$adminpassword = "admin1!";
+$adminname = "admin";
+$username = "user";
+$userpass = "user1!";
+$status = 0;
+if($name == $adminname && $password == $adminpassword){
+	$status = 1;
+	}
+if($name == $username && $password == $userpass){
+	$status = 2;}
+if($status == 1){
+	$user= array('name'=>$name, 'secret'=>'<li class="menu"><a href="personal.php">secretpage</a></li>', 'status'=>1);	
+	$myJSON = json_encode($user);
+	echo $myJSON;
+}
+if($status == 2){
+	$user= array('name'=>$name,'status'=>2);	
+	$myJSON = json_encode($user);
+	echo $myJSON;
+}
+if($status == 0){
+	$user= array('status'=>0);	
+	$myJSON = json_encode($user);
+	echo $myJSON;}
+?>
---
+++ JS_9/personal.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Документ без названия</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+</head>
+
+<body>
+    <div id = "container">
+        <nav>
+        <ul id = "nav">
+        	<li class="menu"><a href="index.html">Main</a></li>
+            <li class="menu">Stuff</li>
+            <li class = "menu">Download</li>
+            <li class = "menu"><a href="personal.php">secret page</a></li>
+        </ul>
+        </nav>
+        <div id="login">
+        <p>Very secretive secret keeper have put their secret secrets on this secret page of secrets.</p>
+            <p id="text"></p>
+        </div>
+    </div>
+</body>
+</html>
